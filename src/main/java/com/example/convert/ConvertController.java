7802eec1f73899107a59ff673f8ca60bc196716e
Piotr Kupis <piotr.kupis42@gmail.com>
2021-11-25 14:35:44 +0100

    add getting data from json

--- src/main/java/com/example/convert/ConvertController.java
+++ src/main/java/com/example/convert/ConvertController.java
@@ -1,14 +1,10 @@
 package com.example.convert;
 
-import java.io.IOException;
-import javax.xml.parsers.ParserConfigurationException;
-import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.xml.sax.SAXException;
 
 @RestController
 public class ConvertController {
@@ -17,26 +13,40 @@ public class ConvertController {
 
     @PostMapping("convert/{inFormat}/{outFormat}")
     public String convert(@PathVariable("inFormat") String inFormat,
-        @PathVariable("outFormat") String outFormat, @RequestBody Text text)
-        throws ParserConfigurationException, IOException, SAXException {
+        @PathVariable("outFormat") String outFormat, @RequestBody Text text) {
 
         String data = text.getText();
         Response response = new Response();
 
         String uppercase = "";
-        String lowercase= "";
-        String specialChars= "";
-        String numbers= "";
-        String combination= "";
+        String lowercase = "";
+        String specialChars = "";
+        String numbers = "";
+        String combination = "";
 
         System.out.println(text);
 
         if (inFormat.equals("XML")) {
-            uppercase = data.substring(data.indexOf("<uppercase>")+"<uppercase>".length(), data.indexOf("</uppercase>"));
-            lowercase = data.substring(data.indexOf("<lowercase>")+"<lowercase>".length(), data.indexOf("</lowercase>"));
-            specialChars = data.substring(data.indexOf("<specialChars>")+"<specialChars>".length(), data.indexOf("</specialChars>"));
-            numbers = data.substring(data.indexOf("<numbers>")+"<numbers>".length(), data.indexOf("</numbers>"));
-            combination = data.substring(data.indexOf("<combination>")+"<combination>".length(), data.indexOf("</combination>"));
+            uppercase = data.substring(data.indexOf("<uppercase>") + "<uppercase>".length(),
+                data.indexOf("</uppercase>"));
+            lowercase = data.substring(data.indexOf("<lowercase>") + "<lowercase>".length(),
+                data.indexOf("</lowercase>"));
+            specialChars = data.substring(
+                data.indexOf("<specialChars>") + "<specialChars>".length(),
+                data.indexOf("</specialChars>"));
+            numbers = data.substring(data.indexOf("<numbers>") + "<numbers>".length(),
+                data.indexOf("</numbers>"));
+            combination = data.substring(data.indexOf("<combination>") + "<combination>".length(),
+                data.indexOf("</combination>"));
+        } else if (inFormat.equals("JSON")) {
+            data = data.replaceAll("\\\\", "");
+            JSONObject jsonObject = new JSONObject(data);
+
+            uppercase = jsonObject.get("uppercase").toString();
+            lowercase = jsonObject.get("lowercase").toString();
+            specialChars = jsonObject.get("specialChars").toString();
+            numbers = jsonObject.get("numbers").toString();
+            combination = jsonObject.get("combination").toString();
         }
 
         System.out.println(uppercase);
